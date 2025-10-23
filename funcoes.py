8360442db342b0fdb5ef05123b7204fb740e5ac8
mateusvbf2006 <mateusvbf@al.insper.edu.br>
2025-10-23 17:02:55 -0300

    Q2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,13 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         for i in range(tamanho):
             l.append([linha, coluna + i])
     return l
+
+
+def preenche_frota(dic_frota, navio, linha, coluna, orientacao, tamanho):
+    dic = dic_frota
+    pos = define_posicoes(linha, coluna, orientacao, tamanho)
+    if navio in dic:
+        dic[navio].append(pos)
+    else:
+        dic[navio] = [pos]
+    return dic
